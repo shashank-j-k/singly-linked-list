6d9b066064bd39264eacbf31936ea64dc53de580
shashank-j-k <shashankjk001@gmail.com>
2025-10-09 16:26:08 +0530

    Some functionalities implemented

--- server.cpp
+++ server.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "server.h"
 
-using std :: cout;
-using std :: endl;
+using std ::cout;
+using std ::endl;
 
 CNode ::CNode()
 {
@@ -23,18 +23,18 @@ CList ::CList()
 
 CList ::CList()
 {
-   if (m_pFirst != NULL)
-   {
+    if (m_pFirst != NULL)
+    {
         DeleteAll();
-   }
+    }
 }
 
-void CList :: InsertAtFirst(int iNo)
+void CList ::InsertAtFirst(int iNo)
 {
     CNode *pNewNode = NULL;
 
     pNewNode = new CNode;
-    if(NULL == pNewNode)
+    if (NULL == pNewNode)
     {
         cout << "Memory Allocation Failed\a\n";
         return;
@@ -45,13 +45,13 @@ void CList :: InsertAtFirst(int iNo)
     m_pFirst = pNewNode;
 }
 
-void CList :: InsertAtLast(int iNo)
+void CList ::InsertAtLast(int iNo)
 {
     CNode *pNewNode = NULL;
     CNode *pTemp = NULL;
 
     pNewNode = new CNode;
-    if(NULL == pNewNode)
+    if (NULL == pNewNode)
     {
         cout << "Memory Allocation Failed\a\n";
         return;
@@ -60,7 +60,7 @@ void CList :: InsertAtLast(int iNo)
     pNewNode->m_iData = iNo;
     pNewNode->m_pNext = NULL;
 
-    if(NULL == m_pFirst)
+    if (NULL == m_pFirst)
     {
         m_pFirst = pNewNode;
         return;
@@ -68,19 +68,19 @@ void CList :: InsertAtLast(int iNo)
 
     pTemp = m_pFirst;
 
-    while(pTemp->m_pNext != NULL)
+    while (pTemp->m_pNext != NULL)
         pTemp = pTemp->m_pNext;
 
     pTemp->m_pNext = pNewNode;
-
 }
-int CList :: DeleteFirst()
+
+int CList ::DeleteFirst()
 {
     int iDelData;
 
     CNode *pTemp = NULL;
 
-    if(NULL == m_pFirst)
+    if (NULL == m_pFirst)
         return -1;
 
     iDelData = m_pFirst->m_iData;
@@ -94,55 +94,142 @@ int CList :: DeleteFirst()
     return iDelData;
 }
 
-int CList :: DeleteLast()
+int CList ::DeleteLast()
 {
+    int iDelData;
 
+    CNode *pTemp = NULL;
+
+    if (NULL == m_pFirst)
+        return -1;
+
+    if (m_pFirst->m_pNext == NULL)
+    {
+        iDelData = m_pFirst->m_iData;
+        delete m_pFirst;
+        m_pFirst = NULL;
+    }
+    else
+    {
+        pTemp = m_pFirst;
+        while (pTemp->m_pNext->m_pNext != NULL)
+            pTemp = pTemp->m_pNext;
+
+        iDelData = pTemp->m_pNext->m_iData;
+        delete pTemp->m_pNext;
+        pTemp->m_pNext = NULL;
+        pTemp = NULL;
+    }
+
+    return iDelData;
 }
-void CList :: InsertAtPosition(int, int)
+void CList ::InsertAtPosition(int, int)
 {
-
 }
-int CList :: DeleteAtPosition(int)
+int CList ::DeleteAtPosition(int)
 {
-
 }
-int CList :: CountNodes()
-{
 
+int CList ::CountNodes()
+{
+    int iCount = 0;
+    CNode *pTemp = m_pFirst;
+
+    while (pTemp != NULL)
+    {
+        iCount++;
+        pTemp = pTemp->m_pNext;
+    }
+
+    return iCount;
 }
-int CList :: SearchFirstOccurance(int)
-{
 
+int CList ::SearchFirstOccurance(int iKey)
+{
+    int iPosition = 0;
+    CNode *pTemp = m_pFirst;
+
+    while (pTemp != NULL)
+    {
+        iPosition++;
+        if(pTemp->m_iData == iKey)
+            return iPosition;
+        pTemp = pTemp->m_pNext;
+    }
+
+    return 0;
 }
-int CList :: SearchLastOccuarnce(int)
-{
 
+int CList ::SearchLastOccuarnce(int iKey)
+{
+    int iPosition = 0;
+    int iLastPosition;
+
+    CNode *pTemp = m_pFirst;
+
+    while (pTemp != NULL)
+    {
+        iPosition ++;
+        if(pTemp->m_iData == iKey)
+            iLastPosition = iPosition;
+        pTemp = pTemp->m_pNext;
+    }
+
+    return iLastPosition;
 }
-int CList :: SearchAllOccuarnces(int)
-{
 
+int CList ::SearchAllOccuarnces(int iKey)
+{
+    int iCount = 0;
+
+    CNode *pTemp = m_pFirst;
+
+    while(pTemp != NULL)
+    {
+        if(iKey == pTemp->m_iData)
+            iCount ++;
+        pTemp = pTemp->m_pNext;
+    }
+
+    return iCount;
 }
-void CList :: ConcatLists()
-{
 
+void CList ::ConcatLists()
+{
 }
-void CList :: ConcatAtPosition()
+void CList ::ConcatAtPosition()
 {
-
 }
-void CList :: Display()
-{
 
+void CList ::Display()
+{
+    CNode *pTemp = m_pFirst;
+
+    while(pTemp != NULL)
+    {
+        cout << "|" << pTemp->m_iData << "|" << "->";
+        pTemp = pTemp->m_pNext;
+    }
+    cout << "|NULL|\n";
 }
-void CList :: ReverseDisplay()
-{
 
+void CList ::ReverseDisplay()
+{
 }
-void CList :: PhysicalReverse()
+void CList ::PhysicalReverse()
 {
-
 }
-void CList :: DeleteAll()
-{
 
+void CList ::DeleteAll()
+{
+    CNode *pTemp = NULL;
+
+    while(m_pFirst != NULL)
+    {
+        pTemp = m_pFirst;
+        m_pFirst = pTemp->m_pNext;
+        pTemp->m_pNext = NULL;
+        delete pTemp;
+        pTemp = NULL;
+    }
 }
